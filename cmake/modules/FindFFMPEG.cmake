cfd166d195f2e7273e60b9dbe07b48e44153efa6
popcornmix <popcornmix@gmail.com>
2020-01-20 16:53:52 +0000

    ffmpeg: Add v4l2 support

--- cmake/modules/FindFFMPEG.cmake
+++ cmake/modules/FindFFMPEG.cmake
@@ -277,7 +277,10 @@ if(NOT FFMPEG_FOUND)
                                     <SOURCE_DIR> &&
                                     ${CMAKE_COMMAND} -E copy
                                     ${CMAKE_SOURCE_DIR}/tools/depends/target/ffmpeg/FindGnuTls.cmake
-                                    <SOURCE_DIR>)
+                                    <SOURCE_DIR> &&
+                                    patch -p1 < ${CMAKE_SOURCE_DIR}/tools/depends/target/ffmpeg/0001-rpi-Add-hevc-acceleration.patch &&
+                                    echo "########################################## patched ffmpeg ##############################"
+                     )
 
   if (ENABLE_INTERNAL_DAV1D)
     add_dependencies(ffmpeg dav1d)
